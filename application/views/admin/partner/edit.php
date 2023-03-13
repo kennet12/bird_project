c8fdd5d05216100700619079bd728d2456006dbf
Quoc Kiet <dinhquockiet170496@gmail.com>
2023-03-13 20:00:21 +0700

    add action use & partner

--- application/views/admin/partner/edit.php
+++ application/views/admin/partner/edit.php
@@ -1,48 +1,56 @@
-<div class="cluster">
-	<div class="container-fluid">
-		<h1 class="page-title">Banner đối tác</h1>
-		<form id="frm-admin" name="adminForm" action="" method="POST" enctype="multipart/form-data">
-			<input type="hidden" id="task" name="task" value="">
-			<table class="table table-bordered">
-				<tr>
-					<td class="table-head text-right" width="10%">Tên đối tác</td>
-					<td><input type="text" id="name" name="name" class="form-control" value="<?=!empty($item->name) ? $item->name : ''?>"></td>
-				</tr>
-				<tr>
-					<td class="table-head text-right" width="10%">URL liên kết</td>
-					<td><input type="text" id="url" name="url" class="form-control" value="<?=!empty($item->url) ? $item->url : ''?>"></td>
-				</tr>
-				<tr>
-					<td class="table-head text-right" width="10%">Hình ảnh</td>
-					<td>
-						<label class="wrap-upload-banner" style="background: url('<?=!empty($item->banner) ? $item->banner : ''?>') no-repeat">
-							<input type="file" name="banner" id="file-upload" value="<?=!empty($item->name) ? $item->name : ''?>">
-							<i class="fa fa-cloud-upload" aria-hidden="true"></i>
-						</label>
-					</td>
-				</tr>
-				<tr>
-					<td class="table-head text-right"></td>
-					<td>
-						<select id="active" name="active" class="form-control">
-							<option value="1">Hiện</option>
-							<option value="0">Ẩn</option>
-						</select>
-						<script type="text/javascript">
-							$("#active").val("<?=$item->active?>");
-						</script>
-					</td>
-				</tr>
-			</table>
-			<div class="text-right">
-				<button class="btn btn-sm btn-primary btn-save">Lưu</button>
-				<button class="btn btn-sm btn-default btn-cancel">Hủy</button>
-			</div>
-		</form>
-		<br>
-	</div>
+<div class="row">
+    <div class="col-12">
+        <div class="card mb-4">
+            <div class="card-header pb-0">
+                <h6><?=$title?></h6>
+            </div>
+            <div class="card-body px-0 pt-0 pb-2">
+				<div class="form-box">
+					<form id="form-post" action="" method="POST">
+						<input type="hidden" name="task" id="task" class="form-control" value="">
+						<div class="row">
+							<div class="col-md-6">
+								<div class="form-group">
+									<div class="input-group">
+										<span class="input-group-text" id="basic-addon1">Tên Đối Tác</span>
+										<input type="text" name="name" value="<?=!empty($partners->name)? $partners->name :''?>" class="form-control">
+									</div>
+									<div class="input-group">
+										<span class="input-group-text" id="basic-addon1">Url</span>
+										<input type="text" name="url" value="<?=!empty($parnerts->url)? $partners->url :''?>" class="form-control">
+									</div>
+									<div class="row">
+										<div class="col-6">
+											<label class="wrap-upload-banner" <?=!empty($partners->banner) ? 'style="background: url('. $partners->banner.')"' : ''?>') no-repeat">
+												<input type="file" name="banner" id="file-upload" value="<?=!empty($partners->name) ? $partners->name : ''?>">
+												<i class="fa fa-cloud-upload" aria-hidden="true"></i>
+											</label>
+										</div>
+										<div class="col-6">
+											<div class="radio">
+												<label>
+													<input type="radio" name="active" id="input" value="1" <?=!empty($parnerts->active) ? 'checked="checked"' : ''?>>
+													Hiện
+												</label>
+											</div>
+											<div class="radio">
+												<label>
+													<input type="radio" name="active" id="input" value="0" <?=empty($parnerts->active) ? 'checked="checked"' : ''?>>
+													Ẩn
+												</label>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+							<button type="button" class="btn-save btn bg-gradient-success">Cập nhật</button>
+					</form>
+				</div>
+            </div>
+        </div>
+    </div>
 </div>
-
 <script>
 $(document).ready(function() {
 	$("#file-upload").change(function() {
@@ -64,7 +72,8 @@ $(document).ready(function() {
 		}
 	}
 	$(".btn-save").click(function(){
-		submitButton("save");
+		$("#task").val('save');
+		$('#form-post').submit();
 	});
 	$(".btn-cancel").click(function(){
 		submitButton("cancel");
--- application/views/admin/partner/index.php
+++ application/views/admin/partner/index.php
@@ -3,6 +3,7 @@
           <div class="card mb-4">
             <div class="card-header pb-0">
               <h6><?=$title?></h6>
+              <a href="<?=site_url('syslog/partners/add')?>"><span class="badge badge-sm bg-gradient-success"><i class="fa-solid fa-plus"></i> Thêm</span></a>
             </div>
             <div class="card-body px-0 pt-0 pb-2">
               <div class="table-responsive p-0">
@@ -13,40 +14,41 @@
                       <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Ngày Hợp Tác</th>
                       <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Trạng Thái</th>
                       <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Ngày Kết Thúc Hợp Đồng</th>
-                      <th class="text-secondary opacity-7"></th>
+                      <th class="text-center text-secondary opacity-7">Hành Động</th>
                     </tr>
                   </thead>
                   <tbody>
                     <?
-                      foreach($partner as $partners) {?>
+                      foreach($partners as $partner) {?>
                   <tr>
                       <td>
                         <div class="d-flex px-2 py-1">
                           <div>
-                            <img src="<?=$partners->banner?>" class="avatar avatar-sm me-3" alt="user1">
+                            <img src="<?=$partner->banner?>" class="avatar avatar-sm me-3" alt="user1">
                           </div>
                           <div class="d-flex flex-column justify-content-center">
-                            <h6 class="mb-0 text-sm"><?=$partners->name?></h6>
+                            <h6 class="mb-0 text-sm"><?=$partner->name?></h6>
                           </div>
                         </div>
                       </td>
                       <td>
-                        <p class="text-xs font-weight-bold mb-0"><?=date('d/m/Y',strtotime($partners->created_date))?></p>
+                        <p class="text-xs font-weight-bold mb-0"><?=date('d/m/Y',strtotime($partner->created_date))?></p>
                       </td>
                       <td class="align-middle text-center text-sm">
-                      <? if ($partners->active == 1)  { ?>
+                      <? if ($partner->active == 1)  { ?>
                         <span class="badge badge-sm bg-gradient-success">Hoạt động</span>
                       <? } else { ?>
                         <span class="badge badge-sm bg-gradient-secondary">Khóa</span>
                       <? } ?>
                       </td>
                       <td class="align-middle text-center">
-                        <span class="text-secondary text-xs font-weight-bold"><?=date('d/m/Y',strtotime($partners->updated_date))?></span>
+                        <span class="text-secondary text-xs font-weight-bold"><?=date('d/m/Y',strtotime($partner->updated_date))?></span>
                       </td>
-                      <td class="align-middle">
-                        <a href="javascript:;" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip" data-original-title="Edit user">
-                          Edit
-                        </a>
+                      <td class="text-center align-middle">
+                      <ul class="action">
+                                        <li><a href="<?=site_url("syslog/partners/edit/{$partner -> id}")?>"><span class="badge badge-sm bg-gradient-info">Sửa</span></a></li>
+                                        <li><a class="btn-delete" linkHref="<?=site_url("syslog/partners/delete/{$partner -> id}")?>"><span class="badge badge-sm bg-gradient-danger">Xóa</span></a></li>
+                       </ul>
                       </td>
                     </tr>
                     <?}?>
@@ -57,3 +59,10 @@
           </div>
         </div>
       </div>
+      <script>
+    $('.btn-delete').click(function(){
+        if (confirm('Bạn có chắc muốn xóa không?') == true) {
+            window.location.href = $(this).attr('linkHref');
+        }
+    })
+</script>
