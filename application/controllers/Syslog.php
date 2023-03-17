e9e08fc9e5fa02cae323f43f5cff46c3b0f3f802
Duy Anh <nevermorepda@gmail.com>
2023-03-17 20:12:08 +0700

    dsad

--- application/controllers/Syslog.php
+++ application/controllers/Syslog.php
@@ -486,77 +486,73 @@ class Syslog extends CI_Controller {
 		}
 		
 	}
-	
-	public function product_category($action=null, $id=null)
-	{
+
+	public function sliders($action=null, $id=null){
 		if(!empty($action))
 		{
 			if(!empty($_POST))
 			{
 				$receive_data=[];
-				$receive_data['name']=$_POST['title'];
-				$receive_data['active']=$_POST['active'];
-
-				if($action =='add')
+				$receive_data['title'] 		 = $_POST['title'];
+				$receive_data['link']		 = $_POST['link'];
+				$receive_data['description'] = $_POST['description'];
+				$receive_data['thumbnail']   = $_POST['thumbnail'];
+				$receive_data['active'] 	 = $_POST['active'];
+				if($action == 'add')
 				{
-					if ($action == "add") {
-						$this->m_product_categories->add($receive_data);
-						$this->session->set_flashdata("success", "Thêm danh mục thành công");
-					}
-
+					$this->session->set_flashdata("success", "Thêm thành công");
+					$this->m_slide->add($receive_data);
+					
 				}
-				if($action=='edit')
+				else if($action =='edit')
 				{
-					$this->m_product_categories->update($receive_data,['id'=>$id]);
-					$this->session->set_flashdata("success", "Cập nhật danh mục thành công");
-
+					$this->m_slide->update($receive_data,['id'=>$id]);
+					$this->session->set_flashdata("success", "Cập nhật thành công");
+					
 				}
-				redirect(site_url("syslog/product_category"), "back");
+				redirect(site_url("syslog/sliders"), "back");
 
 			}
-
-			if($action =='add')
+			// load giao diện
+			if($action == 'add')
 			{
 				$view_data = array();
-				$view_data["title"] = 'Thêm Danh Mục';
-
-				$tmpl_product_categories = array();
-				$tmpl_product_categories["content"] = $this->load->view("admin/product/category/edit", $view_data, true);
-				$this->load->view("layout/admin/main", $tmpl_product_categories);
-			}
-			else if($action =='edit')
-			{
-				$kq_product_category = $this->m_product_categories->load($id);
-				$view_data = array();
-				$view_data["title"] = 'Cập Nhật Danh Mục';
-				$view_data["product_category_chuyen"] = $kq_product_category;
-
-
-				$tmpl_product_categories = array();
-				$tmpl_product_categories["content"] = $this->load->view("admin/product/category/edit", $view_data, true);
-				$this->load->view("layout/admin/main", $tmpl_product_categories);
-			}
-			else if($action=='delete'){
-
-				$this->m_product_categories->remove(['id' => $id]);
+				$view_data["title"] = 'Thêm mới Slider';
 				
+				$tmpl_slider = array();
+				$tmpl_slider["content"] = $this->load->view("admin/slide/edit", $view_data, true);
+				$this->load->view("layout/admin/main", $tmpl_slider);
+			}
+			else if($action == 'edit')
+			{
+				
+				$kq_slider_item = $this->m_slide->load($id);
+				$view_data = array();
+				$view_data["slider_chuyen_item"] = $kq_slider_item;
+				$view_data["title"] = 'Cập nhật Slider';
+				
+				$tmpl_slider = array();
+				$tmpl_slider["content"] = $this->load->view("admin/slide/edit", $view_data, true);
+				$this->load->view("layout/admin/main", $tmpl_slider);	
+			}
+			else if($action == 'delete')
+			{
+				$this->m_slide->remove(['id' => $id]);
 				$this->session->set_flashdata("success", "Xóa thành công");
-				redirect(site_url("syslog/product_category"), "back");
-
+				redirect(site_url("syslog/sliders"), "back");
 			}
 		}
-		else{
-			$kq_product_category = $this->m_product_categories->items();
+		else
+		{
+			$kq_slider = $this->m_slide->items();
 			$view_data = array();
-			$view_data["product_category_chuyen"] = $kq_product_category;
-			$view_data["title"] = 'Danh sách Danh Mục';
+			$view_data["slider_chuyen"] = $kq_slider;
+			$view_data["titles"] = 'Danh sách Slider';
 
-			$tmpl_product_categories = array();
-			$tmpl_product_categories["content"] = $this->load->view("admin/product/category/index", $view_data, true);
-			$this->load->view("layout/admin/main", $tmpl_product_categories);
+			$tmpl_content = array();
+				$tmpl_content["content"] = $this->load->view("admin/slide/index", $view_data, true);
+				$this->load->view("layout/admin/main", $tmpl_content);
 		}
-
-		
 	}
 
 	public function sliders($action=null, $id=null){
--- application/views/admin/slide/edit.php
+++ application/views/admin/slide/edit.php
@@ -4,37 +4,34 @@
         <div class="card mb-4">
             <div class="card-header pb-0">
                 <h6><?=$title;?></h6>
-            </div> 
+            </div>
             <div class="card-body px-0 pt-0 pb-2">
 				<div class="form-box">
-					<form id="form-post" action="" method="POST" enctype="multipart/form-data">
+					<form id="form-post" action="" method="POST">
 						<input type="hidden" name="task" id="task" class="form-control" value="">
 						<div class="row">
 						<div class="col-md-1">
 						</div>
 							<div class="col-md-10">
 								<div class="form-group">
-									<div class="input-group invalid">
+									<div class="input-group">
 										<span class="input-group-text" id="basic-addon1">Tiêu đề</span>
-										<input type="text" name="title" id="title" value="<?=!empty($slider_chuyen_item->title)? $slider_chuyen_item->title :''?>" class="form-control">
-										<span class="form-message"></span>
+										<input type="text" name="title" value="<?=!empty($slider_chuyen_item->title)? $slider_chuyen_item->title :''?>" class="form-control">
 									</div>
-									
-
 									<div class="input-group">
 										<span class="input-group-text" id="basic-addon1">link liên kết</span>
-										<input type="text" id="link" name="link" value="<?=!empty($slider_chuyen_item->link)? $slider_chuyen_item->link:'' ?>" class="form-control">
+										<input type="text" name="link" value="<?=!empty($slider_chuyen_item->link)? $slider_chuyen_item->link:'' ?>" class="form-control">
 									</div>
 									<div class="form-group">
 										<div class="input-group">
-											<textarea class="form-control" id="description" name="description" placeholder="Mô tả" rows="5"><?=!empty($slider_chuyen_item->description)? $slider_chuyen_item->description :''?></textarea>
+											<textarea class="form-control" name="description" placeholder="Mô tả" rows="5"><?=!empty($slider_chuyen_item->description)? $slider_chuyen_item->description :''?></textarea>
 										</div>
 									</div>
 									
 									
 									<div class="row">
 									<div class="col-6">
-									<label class="wrap-upload-banner" <?=!empty($slider_chuyen_item->thumbnail) ? 'style="background: url('.BASE_URL. $slider_chuyen_item->thumbnail.')"' : ''?>') no-repeat">
+											<label class="wrap-upload-banner" <?=!empty($slider_chuyen_item->thumbnail) ? 'style="background: url('. $slider_chuyen_item->thumbnail.')"' : ''?>') no-repeat">
 												<input type="file" name="thumbnail" id="file-upload" value="<?=!empty($slider_chuyen_item->title) ? $slider_chuyen_item->title : ''?>">
 												<i class="fa fa-cloud-upload" aria-hidden="true"></i>
 											</label>
@@ -61,7 +58,7 @@
 						<div class="col-md-1">
 						</div>
 						<div class="text-center">
-							<button type="button" id="btn-save" class="btn-save btn bg-gradient-success">Cập nhật</button>
+							<button type="button" class="btn-save btn bg-gradient-success">Cập nhật</button>
 						</div>
 					</form>
 				</div>
@@ -148,27 +145,9 @@ $(document).ready(function() {
 		}
 	}
 	$(".btn-save").click(function(){
-		var error = [];
-
-			if ($('#title').val() == '') {
-				error.push('Vui lòng nhập tên tiêu đề.')
-			}
-
-			if ($('#link').val() == '') {
-				error.push('Vui lòng nhập Link')
-			}
-			if ($('#description').val() == '') {
-				error.push('Vui lòng nhập nội dung.')
-			}
-
-			if (error.length == 0) {
-				$("#task").val('save');
-				$('#form-post').submit();
-			} else {
-				messageBox('error','Đã xảy ra lỗi',error);
-			}
-
-			});
+		$("#task").val('save');
+		$('#form-post').submit();
+	});
 	$(".btn-cancel").click(function(){
 		submitButton("cancel");
 	});
